345c4ba27d7bebf8ca3f7544beb0b018d1e7883e
a_total_another_bla <some@email.org>
2018-05-06 17:51:02 +0200

    setup maven

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+./switch_JVM_version.sh 8
+
 mvn -N io.takari:maven:wrapper -Dmaven=3.5.3
 
 echo $JAVA_HOME
@@ -8,6 +10,8 @@ java -version
 javac -version
 ./mvnw -version
 
-#./mvnw clean install site -DskipTests=true
+#./mvnw -U -e -V clean install site -DskipTests=true
 
-./mvnw verify
+./mvnw -U -e -V verify
+
+./mvnw  -U -e -V dependency:tree
