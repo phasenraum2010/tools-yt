1eddef8b5db8c15596fb621c4c72e98f522f2dab
a_total_another_bla <some@email.org>
2018-05-07 18:25:11 +0200

    setup maven

--- yt-audiobooks/yt-audiobooks-clickdummy/src/main/java/org/woehlke/tools/yt/audiobooks/clickdummy/YtAudiobooksClickdummyApplication.java
+++ yt-audiobooks/yt-audiobooks-clickdummy/src/main/java/org/woehlke/tools/yt/audiobooks/clickdummy/YtAudiobooksClickdummyApplication.java
@@ -2,12 +2,11 @@ package org.woehlke.tools.yt.audiobooks.clickdummy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.woehlke.tools.yt.audiobooks.terryprattchet.YtAudiobooksTerryprattchetApplication;
 
 @SpringBootApplication
 public class YtAudiobooksClickdummyApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(YtAudiobooksTerryprattchetApplication.class, args);
+        SpringApplication.run(YtAudiobooksClickdummyApplication.class, args);
     }
 }
