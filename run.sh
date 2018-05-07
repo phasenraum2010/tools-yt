a8a55fd876defe871b9a97c464570f96da0d0a18
a_total_another_bla <some@email.org>
2018-05-07 16:38:50 +0200

    setup maven

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 source "$HOME/.sdkman/bin/sdkman-init.sh"
 
-mvn -N io.takari:maven:wrapper -Dmaven=3.3.9
+mvn -amd io.takari:maven:wrapper -Dmaven=3.3.9
 
 echo "----------------------"
 echo $JAVA_HOME
