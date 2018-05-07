79f228a48f48d8d5b83760fadfe33006f4c0a1e4
phasenraum2010 <Recoil89>
2018-05-07 16:24:24 +0200

    maven setup windows10

--- run.cmd
+++ run.cmd
@@ -1,17 +1,19 @@
-mvn -N io.takari:maven:wrapper -Dmaven=3.5.3
+@REM mvn -N io.takari:maven:wrapper -Dmaven=3.3.9
 
 echo "----------------------"
 echo %JAVA_HOME%
+echo %M2_HOME%
+echo %M2_REPO%
 echo "----------------------"
 java -version
 echo "----------------------"
 javac -version
 echo "----------------------"
-./mvnw -version
+@REM mvnw -version
 echo "----------------------"
-./mvnw -U -e -V verify
+mvnw -U -e -V verify
 echo "----------------------"
-./mvnw  -U -e -V clean install dependency:tree -DskipTests=true
+mvnw -U -e -V clean install dependency:tree -DskipTests=true
 echo "----------------------"
-./mvnw -U -e -V clean install site -DskipTests=true
+mvnw -U -e -V clean install site -DskipTests=true
 
