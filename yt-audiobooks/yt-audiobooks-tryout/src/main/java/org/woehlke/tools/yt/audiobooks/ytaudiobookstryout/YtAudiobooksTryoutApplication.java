b40fad3cc113e015eea553c4e70682343c2625b1
a_total_another_bla <some@email.org>
2018-05-07 18:06:06 +0200

    setup maven

---
+++ yt-audiobooks/yt-audiobooks-tryout/src/main/java/org/woehlke/tools/yt/audiobooks/ytaudiobookstryout/YtAudiobooksTryoutApplication.java
@@ -0,0 +1,12 @@
+package org.woehlke.tools.yt.audiobooks.ytaudiobookstryout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YtAudiobooksTryoutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YtAudiobooksTryoutApplication.class, args);
+	}
+}
